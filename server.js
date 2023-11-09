fb5055deea800359737dd1dbcb0466edf1d6d82e
WNicoleS <133277288+WNicoleS@users.noreply.github.com>
2023-11-09 07:36:31 -0600

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const { readFromFile, readAndAppend } = require('./helpers/fsUtils');
 
 const dbData = require("./db/db.json");
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
